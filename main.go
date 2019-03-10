d8705d800a47369a8f970596973567b2beaef2cb
Yuriy Bazunov <bazunov.yury@gmail.com>
2019-03-10 17:49:39 +0300

    Test program.

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/brian-armstrong/gpio"
+	"log"
+	"time"
+)
+
+func main() {
+
+	for {
+		var i uint = 0
+		for ; i < 26; i++ {
+			pin := gpio.NewOutput(i, false)
+			if err := pin.High(); err != nil {
+				log.Printf("Can't set high value to %d pin", pin.Number)
+			}
+			time.Sleep(time.Second / 2)
+			if err := pin.Low(); err != nil {
+				log.Printf("Can't set low value to %d pin", pin.Number)
+			}
+		}
+		time.Sleep(time.Second / 2)
+	}
+}
