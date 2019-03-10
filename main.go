0d4959f0c16d26acac79c0a25e25fd2dd1975610
Yuriy Bazunov <bazunov.yury@gmail.com>
2019-03-10 18:02:22 +0300

    Test program.

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func main() {
 		for ; i < 26; i++ {
 			pin := gpio.NewOutput(i, false)
 			if err := pin.High(); err != nil {
-				log.Printf("Can't set high value to %d pin", pin.Number)
+				log.Printf("Can't set high value to %d pin: %v", pin.Number, err)
 			}
 			time.Sleep(time.Second / 2)
 			if err := pin.Low(); err != nil {
-				log.Printf("Can't set low value to %d pin", pin.Number)
+				log.Printf("Can't set low value to %d pin: %v", pin.Number, err)
 			}
 		}
 		time.Sleep(time.Second / 2)
